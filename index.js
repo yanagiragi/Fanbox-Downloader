0466a376c8489f27e741b7d0676e886cc475e823
yanagiragi <qaz90473@gmail.com>
2021-07-06 23:24:20 +0800

    refactor: Add downloading covers & posts with no images
    - Now it downloads posts with no images, so the amount of the folder in Storage/${id} will be same amount as document.querySelectorAll('.ar8j54-4.jmnhBW')

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 const path = require('path')
 const util = require('util')
+const sanitize = require("sanitize-filename");
 const streamPipeline = util.promisify(require('stream').pipeline)
 
 const id = process.env.id
@@ -32,6 +33,8 @@ async function main()
             return container
         }
 
+        console.log(`Fetching ${url}`)
+
         try {
             const resp = await FetchWrapper(url)
             const data = await resp.json()
@@ -40,18 +43,18 @@ async function main()
                 return container
             }
     
-            const posts = data.body.items.map(x => {            
-                return {
-                    'title': x.title,
-                    'images': [ 
-                        x.body?.images?.map(el => el.originalUrl), 
-                        Object.values(x.body?.imageMap ?? []).map(el => el.originalUrl),
-                        x.body?.files?.map(x => x.url), 
-                    ].filter(Boolean).flat()
-                }
-            })
+            const posts = data.body.items.map(x => ({                
+                'id': x.id,
+                'title': x.title,
+                'cover': x.coverImageUrl,
+                'images': [ 
+                    x.body?.images?.map(el => el.originalUrl), 
+                    Object.values(x.body?.imageMap ?? []).map(el => el.originalUrl),
+                    x.body?.files?.map(x => x.url), 
+                ].filter(Boolean).flat()                
+            }))
     
-            return fetchInternal(data.nextUrl, [posts.filter(x => x.images.length > 0), container].flat())
+            return fetchInternal(data.body.nextUrl, [posts, container].flat())
         } catch (error) {
             console.error("Error Occurs.")
             console.error(`Raw = ${error}`)
@@ -61,7 +64,7 @@ async function main()
 
     const results = await fetchInternal(`https://api.fanbox.cc/post.listCreator?creatorId=${id}&limit=100`, [])
 
-    return await Deal(results)    
+    await Deal(results)    
 }
 
 async function FetchImage (url, filename) {
@@ -79,35 +82,51 @@ async function FetchImage (url, filename) {
 	return true
 }
 
+async function Download(image, filename)
+{
+    if (fs.existsSync(filename)) {
+        console.log(`\t\t Skiped: ${image}`)
+        return
+    }
+    const result = await FetchImage(image, filename)
+    if (result)
+        console.log(`\t\t Downloaded: ${image}`)
+    else
+        console.log(`\t\t Error: ${image}`)
+}
+
 async function Deal(results) {
 
     if (!fs.existsSync('Storage')) {
         fs.mkdirSync('Storage')
     }
 
-    for(const result of results) {
+    if (!fs.existsSync(path.join('Storage', id))) {
+        fs.mkdirSync(path.join('Storage', id))
+    }
+
+    for (const result of results) {
         console.log(`Download ${result.title} [${result.images.length}]`)
 
-        const folderPath = path.join('Storage', result.title)
+        const folderPath = path.join('Storage', id, `${result.id}-${sanitize(result.title)}`)
         if (!fs.existsSync(folderPath)) {
             fs.mkdirSync(folderPath)
         }
 
+        if (result.cover) {
+            console.log(`\t Downloading Cover: ${JSON.stringify(result)}`)
+            const matched = result.cover.match(/\/([a-zA-Z0-9]+\.[a-zA-Z0-9]+$)/)
+            const savename = matched[0]
+            const filename = path.join(folderPath, `cover-${savename.substring(1)}`)
+            await Download(result.cover, filename)
+        }
+
         for(const image of result.images) {
-            console.log(`\t Downloading ${image}`)            
+            console.log(`\t Downloading ${image}`)
             const matched = image.match(/\/([a-zA-Z0-9]+\.[a-zA-Z0-9]+$)/)
             const savename = matched[0]
             const filename = path.join(folderPath, savename.substring(1))
-
-            if (fs.existsSync(filename)) {
-                console.log(`\t\t Skiped: ${image}`)
-                continue
-            }
-            const result = await FetchImage(image, filename)
-            if (result)
-                console.log(`\t\t Downloaded: ${image}`)
-            else
-                console.log(`\t\t Error: ${image}`)
+            await Download(image, filename)
         }
     }
 }
